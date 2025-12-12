17bd8316292759d82f67168cf6b8a7bd57bb381d
mathcmk <mathcmk@hotmail.com>
2025-12-11 22:07:49 -0300

    Update Calculador.java

--- src/Calculador.java
+++ src/Calculador.java
@@ -1,8 +1,7 @@
 import java.time.Duration;
 
 public class Calculador {
-    private double quantHorasExtrasTotais;
-
+   
     public double valorHorasExtrasTotalUteis(double salario, double horasMes, double horasExtras) {
         double valorDaHora = salario /  horasMes;
         return (valorDaHora * 1.5) * horasExtras;
